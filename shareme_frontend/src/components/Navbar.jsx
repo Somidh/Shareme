656cbc76d65e5b3a71300f692f06bd9f9dd6a284
somidhroy <somidhroy@gmail.com>
2023-03-31 19:01:08 +0530

    added pin card and feed section

--- shareme_frontend/src/components/Navbar.jsx
+++ shareme_frontend/src/components/Navbar.jsx
@@ -9,11 +9,11 @@ const Navbar = ({ searchTerm, setSearchTerm, user }) => {
 
   return (
     <div className="flex gap-2 md:gap-5 w-full mt-5 pb-7">
-      <div className="flex justify-start items-center w-full px-2 rounded-md bg-white border-none outline-none focus-within:shadow-sm">
-        <IoMdSearch fontSize={21} className="ml-1" />
+      <div className="flex justify-start items-center w-full px-2 rounded-md bg-white border-none focus-within:shadow-sm outline-none ">
+        <IoMdSearch className="ml-1 text-xl" />
         <input
           type="text"
-          onChange={(e) => e.target.value}
+          onChange={(e) => setSearchTerm(e.target.value)}
           placeholder="Search"
           value={searchTerm}
           onFocus={() => navigate("/search")}
