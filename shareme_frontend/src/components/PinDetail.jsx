52f30d4a35957e5027378c211ac1b9331b1016eb
somidhroy <somidhroy@gmail.com>
2023-03-31 17:06:11 +0530

    updated navbar and added routes for pins section

---
+++ shareme_frontend/src/components/PinDetail.jsx
@@ -0,0 +1,12 @@
+ import React from 'react'
+ 
+ const PinDetail = () => {
+   return (
+     <div>
+       
+     </div>
+   )
+ }
+ 
+ export default PinDetail
+ 
--- shareme_frontend/src/container/Pins.jsx
+++ shareme_frontend/src/container/Pins.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import { Navbar, PinDetail, CreatePin, Search, Feed } from "../components";
 
-const Pins = () => {
-  return <div></div>;
+const Pins = ({ user }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  return (
+    <div className="px-2 md:px-5 ">
+      <div className="bg-gray-50">
+        <Navbar
+          searchTerm={searchTerm}
+          setSearchTerm={setSearchTerm}
+          user={user}
+        />
+      </div>
+
+      <div className="h-full">
+        <Routes>
+          <Route path="/" element={<Feed />} />
+          <Route path="/create-pin" element={<CreatePin />} />
+          <Route path="/category/:categoryId" element={<Feed />} />
+          <Route
+            path="/pin-detail/:pinId"
+            element={<PinDetail user={user} />}
+          />
+          <Route
+            path="/search"
+            element={
+              <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+            }
+          />
+        </Routes>
+      </div>
+    </div>
+  );
 };
 
 export default Pins;
